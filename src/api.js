b9e06719d2d4eaa0698c7285b31ababbd437600e
Alekskarb <korbutowski@gmail.com>
2020-02-28 10:43:29 +0300

    refactoring2

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://social-network.samuraijs.com/api/1.0/todo-lists",
-    withCredentials: true,                                       // передавай с запросом куки для запрашиваемого домена
+    baseURL: "https://social-network.samuraijs.com/api/1.1/todo-lists",
+    withCredentials: true,
     headers: {"API-KEY": '3eb442be-c6ec-48c8-96fc-69807a29300c'} // специальный ключ в заголовках передаём
-});
+});// передавай с запросом куки для запрашиваемого домена
 
 export const api = {
     createTask(newTaskTitle, todolistId) {
@@ -16,14 +16,14 @@ export const api = {
     getTodolists() {
         return instance.get("");
     },
-    updateTask(task) {
-        return instance.put(`/tasks`, task);
+    updateTask(taskId, todolistId, task) {
+        return instance.put(`/${todolistId}/tasks/${taskId}`, task);
     },
     deleteTodolist(id) {
         return instance.delete("/" + id)
     },
-    deleteTask(id) {
-        return instance.delete(`/tasks/${id}`)
+    deleteTask(todolistId, taskId) {
+        return instance.delete(`/${todolistId}/tasks/${taskId}`)
     },
     getTasks(todolistId) {
         return instance.get(`/${todolistId}/tasks`)
@@ -31,7 +31,7 @@ export const api = {
     updateTodolistTitle(title, todolistId) {
         return instance.put(`/${todolistId}`, {title: title})
     }
-}
+};
 
 
 
